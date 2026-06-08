ed87861495d04ec6c27bc76d7e475ac5a8fe4de0
Moeez ul hassan <moeezjutt11@gmail.com>
2026-06-08 23:47:31 +0500

    fix: resolved security vulnerabilities identified by SonarCloud

--- app/database/database.py
+++ app/database/database.py
@@ -3,10 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 # 1. Fetch the URL from Docker environment variables, or fallback to localhost if running manually
-SQLALCHEMY_DATABASE_URL = os.getenv(
-    "DATABASE_URL", 
-    "mysql+pymysql://root:@127.0.0.1:3306/cart"
-)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set!")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
